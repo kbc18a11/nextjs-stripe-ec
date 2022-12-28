129b20318aebc255bbbbba3fb91d38d327d15b1f
kbc18a11 <wada121400@gmail.com>
2022-12-28 18:23:21 +0900

    https://zenn.dev/stripe/books/stripe-nextjs-use-shopping-cart/viewer/step1-2_nextjs_ssr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Navbar, Container } from 'react-bootstrap'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            Hello EC
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+export default MyApp
